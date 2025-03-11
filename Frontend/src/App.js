34e4072fc95d05c2484d17d8dbc98901abeb8628
Nafeel <nafeel501@gmail.com>
2025-03-11 16:14:54 +0500

    Initial commit with frontend and backend

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LandingPage from './components/landing_page';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <LandingPage />
+      
+    </>
+  );
+}
+
+export default App;
