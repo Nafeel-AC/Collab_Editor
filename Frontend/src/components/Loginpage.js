b541524b327e278f262b2636ac50c31c540a6a0f
Nafeel <nafeel501@gmail.com>
2025-03-12 14:48:51 +0500

    Added animations in project and implemented dark theme

--- Frontend/src/components/Loginpage.js
+++ Frontend/src/components/Loginpage.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -11,22 +12,23 @@ const LoginPage = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
-      <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
+    <div className="min-h-screen bg-gray-900 text-gray-100 flex justify-center">
+      <div className="max-w-screen-xl m-0 sm:m-10 bg-gray-800 shadow sm:rounded-lg flex justify-center flex-1">
         <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
           <div>
-            <img
-              src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-              alt="Logo"
-              className="w-32 mx-auto"
+            <DotLottieReact
+              src="https://lottie.host/773ce559-7fb2-4f69-ac91-e15477f422dc/aKNAIp3GK6.lottie"
+              loop
+              autoplay
+              style={{ width: "300px", height: "300px" }} // Adjust size here
             />
           </div>
           <div className="mt-12 flex flex-col items-center">
-            <h1 className="text-2xl xl:text-3xl font-extrabold">Sign In</h1>
+            <h1 className="text-2xl xl:text-3xl font-extrabold text-gray-400">Sign In</h1>
             <div className="w-full flex-1 mt-8">
               <form className="mx-auto max-w-xs" onSubmit={handleSubmit}>
                 <input
-                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white"
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-700 border border-gray-600 placeholder-gray-400 text-sm focus:outline-none focus:border-gray-500 focus:bg-gray-600"
                   type="email"
                   placeholder="Email"
                   value={email}
@@ -34,7 +36,7 @@ const LoginPage = () => {
                   required
                 />
                 <input
-                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-100 border border-gray-200 placeholder-gray-500 text-sm focus:outline-none focus:border-gray-400 focus:bg-white mt-5"
+                  className="w-full px-8 py-4 rounded-lg font-medium bg-gray-700 border border-gray-600 placeholder-gray-400 text-sm focus:outline-none focus:border-gray-500 focus:bg-gray-600 mt-5"
                   type="password"
                   placeholder="Password"
                   value={password}
@@ -45,22 +47,20 @@ const LoginPage = () => {
                   type="submit"
                   className="mt-5 tracking-wide font-semibold bg-indigo-500 text-gray-100 w-full py-4 rounded-lg hover:bg-indigo-700 transition-all duration-300 ease-in-out flex items-center justify-center focus:outline-none focus:shadow-outline"
                 >
-                  <span className="ml-3">Sign In</span>
+                  <span className="ml-3 font-bold text-gray-400">Sign In</span>
                 </button>
               </form>
-              <Link to="/SignupPage" className="mt-4 text-sm text-indigo-500 hover:underline">
+              <Link
+                to="/SignupPage"
+                className="mt-4 font-bold text-gray-400 hover:underline"
+              >
                 Don't have an account? Sign Up
               </Link>
-              <p className="mt-6 text-xs text-gray-600 text-center">
-                I agree to abide by templatana's
-                <a href="#" className="border-b border-gray-500 border-dotted"> Terms of Service </a>
-                and its
-                <a href="#" className="border-b border-gray-500 border-dotted"> Privacy Policy </a>
-              </p>
+             
             </div>
           </div>
         </div>
-        <div className="flex-1 bg-indigo-100 text-center hidden lg:flex">
+        <div className="flex-1 bg-gray-700 text-center hidden lg:flex">
           <div
             className="m-12 xl:m-16 w-full bg-contain bg-center bg-no-repeat"
             style={{
